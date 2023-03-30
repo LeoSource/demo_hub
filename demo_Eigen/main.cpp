40821ddc0deab5e019a8d236deecb9e26864956d
zxliao <zhixiangleo@163.com>
2023-03-30 16:09:11 +0800

    fix：eigen测试用例

--- demo_Eigen/main.cpp
+++ demo_Eigen/main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include "test_eigen.h"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char**) 
+{
+    
+    //// test transformation between eigen matrix and array/vector
+    //array2eigenMat();
+    //eigenMat2array();
+
+    //array2eigenVec();
+    //eigenVec2array();
+
+    //vector2eigenMat();
+    //eigenMat2vector();
+
+    //vector2eigenVec();
+    //eigenVec2vector();
+
+    test_eigen::test_random_matrix();
 }
