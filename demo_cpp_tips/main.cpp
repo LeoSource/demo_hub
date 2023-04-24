713994a99cadeed32d5dff7c375bb9009e1a4157
zxliao <zhixiangleo@163.com>
2023-04-24 16:39:45 +0800

    feat：cmake中通过add_definitions函数的宏定义来实现源码中的逻辑

--- demo_cpp_tips/main.cpp
+++ demo_cpp_tips/main.cpp
@@ -6,4 +6,9 @@ int main(int, char**)
     // test_other::test_fill();
     // test_other::convert_string();
     test_other::bind_function();
+#ifdef SIMULATION
+    std::cout<<"simulation mode"<<std::endl;
+#else
+    std::cout<<"runtime mode"<<std::endl;
+#endif
 }
