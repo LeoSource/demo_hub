de25fa0eb587fd454b256cb2a496046eca6bc881
zxliao <zhixiangleo@163.com>
2024-03-26 17:59:36 +0800

    chore：设置interactor和interactorstyle的顺序很关键

--- demo_python/dicom_viewer.py
+++ demo_python/dicom_viewer.py
@@ -5,71 +5,64 @@
 # @Author	    : zxliao, zhixiangleo@163.com
 
 import vtk
+import SimpleITK as sitk
 
 dcm_path = "F:/0_project/prca/dicom/20240225/2024.02.25-144314-STD-1.3.12.2.1107.5.99.3/20240225/1.3.12.2.1107.5.1.7.120479.30000024022512255527200003523"
 
+def sitk_read_dcm_series(file_path:str):
+    reader = sitk.ImageSeriesReader()
+    dcm_names = reader.GetGDCMSeriesFileNames(file_path)
+    reader.SetFileNames(dcm_names)
+    return reader.Execute()
+
+colors = vtk.vtkNamedColors()
+
 class CustomInteractorStyle(vtk.vtkInteractorStyleImage):
     def __init__(self, img_viewer):
-        self.AddObserver(vtk.vtkCommand.MouseMoveEvent, self.on_mouse_move)
         self.AddObserver(vtk.vtkCommand.MouseWheelForwardEvent, self.on_scroll_forward)
         self.AddObserver(vtk.vtkCommand.MouseWheelBackwardEvent, self.on_scroll_backward)
-        # self.AddObserver(vtk.vtkCommand.LeftButtonPressEvent, self.on_left_button_press)
-        # self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, self.on_left_button_release)
+        self.AddObserver(vtk.vtkCommand.LeftButtonPressEvent, self.on_left_button_press)
+        self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, self.on_left_button_release)
+        self.AddObserver(vtk.vtkCommand.MouseMoveEvent, self.on_mouse_move)
         # self.AddObserver("RightButtonPressEvent", self.onRightButtonDown)
         # self.AddObserver("RightButtonReleaseEvent", self.onRightButtonUp)
         # self.AddObserver("MiddleButtonPressEvent", self.onMiddleButtonDown)
         # self.AddObserver("MiddleButtonReleaseEvent", self.onMiddleButtonUp)
 
         self.img_viewer = img_viewer
+        self.sitk_img = sitk_read_dcm_series(dcm_path)
 
-        self.lastPickedActor = None
-        self.lastPickedPosition = [0, 0, 0]
-
-        self.windowWidth = 400
-        self.windowLevel = 40
+        self.slice_max = img_viewer.GetSliceMax()
+        self.slice_min = img_viewer.GetSliceMin()
+        self.slice = round(0.5*(self.slice_min+self.slice_max))
+        self.img_viewer.SetSlice(self.slice)
+        self.wl = img_viewer.GetColorLevel()
+        self.wd = img_viewer.GetColorWindow()
 
         self.isLeftButtonDown = False
         self.isRightButtonDown = False
-        self.isMiddleButtonDown = False
-        self.startPosition = [0, 0]
 
     def on_mouse_move(self, obj, event):
-        interactor = self.GetInteractor()
-        x, y = interactor.GetEventPosition()
-
-        picker = vtk.vtkCellPicker()
-        picker.SetTolerance(0.001)
-        picker.Pick(x, y, 0, self.GetDefaultRenderer())
-        pos = picker.GetPickPosition()
-        print(pos)
-        # print(interactor.GetEventPosition())
-
-        if self.isLeftButtonDown:
-            # Adjust window level based on mouse movement
-            dx = x - self.startPosition[0]
-            dy = y - self.startPosition[1]
-            self.windowLevel += dy
-            self.windowWidth += dx
-            print(f"WL: {self.windowLevel}, WD: {self.windowWidth}")
-        elif self.isMiddleButtonDown:
-            # Move the camera to simulate image panning
-            camera = self.GetDefaultRenderer().GetActiveCamera()
-            camera.Azimuth(self.startPosition[0] - x)
-            camera.Elevation(y - self.startPosition[1])
-            camera.OrthogonalizeViewUp()
-            self.startPosition = [x, y]
+        print('mouse move')
         super().OnMouseMove()
-        # self.GetInteractor().Render()
 
     def on_scroll_forward(self, obj, event):
-        print("Scrolling forward")
-        # Increase slice number or zoom in
+        print('scroll forward')
+        # super().OnMouseWheelForward()
+        if self.slice<self.slice_max:
+            self.slice += 1
+            self.img_viewer.SetSlice(self.slice)
+            self.img_viewer.Render()
 
     def on_scroll_backward(self, obj, event):
-        print("Scrolling backward")
-        # Decrease slice number or zoom out
+        # super().OnMouseWheelBackward()
+        if self.slice>self.slice_min:
+            self.slice -= 1
+            self.img_viewer.SetSlice(self.slice)
+            self.img_viewer.Render()
 
     def on_left_button_press(self, obj, event):
+        print('left button pressed')
         self.isLeftButtonDown = True
         self.startPosition = self.GetInteractor().GetEventPosition()
         super().OnLeftButtonDown()
@@ -98,16 +91,21 @@ class CustomInteractorStyle(vtk.vtkInteractorStyleImage):
     #     super().OnMiddleButtonUp()
 
 class DICOMViewer():
-    def __init__(self,dcm_path):
+    def __init__(self,dcm_path:str):
         reader = vtk.vtkDICOMImageReader()
         reader.SetDirectoryName(dcm_path)
         reader.Update()
+        # vtk_img_data = reader.GetOutput()
+        flip = vtk.vtkImageFlip()
+        flip.SetFilteredAxes(2)
+        flip.SetInputConnection(reader.GetOutputPort())
+        flip.Update()
 
+        # rens = [vtk.vtkRenderer() for _ in range(4)]
         ren_windows = [vtk.vtkRenderWindow() for _ in range(4)]
-        rens = [vtk.vtkRenderer() for _ in range(4)]
         # irens = [vtk.vtkRenderWindowInteractor() for _ in range(4)]
         for idx,window in enumerate(ren_windows):
-            window.AddRenderer(rens[idx])
+            # window.AddRenderer(rens[idx])
             window.SetSize(512,512)
             window.SetPosition(512*(idx%2),512*(idx//2))
 
@@ -118,12 +116,15 @@ class DICOMViewer():
         iren = vtk.vtkRenderWindowInteractor()
 
         self.view_axial = vtk.vtkImageViewer2()
-        self.view_axial.SetInputConnection(reader.GetOutputPort())
+        self.view_axial.SetInputConnection(flip.GetOutputPort())
+        self.view_axial.GetRenderer().SetBackground(colors.GetColor3d('gray'))
         self.view_axial.SetRenderWindow(ren_windows[0])
         self.view_axial.GetRenderWindow().SetWindowName("Axial")
-        self.view_axial.SetupInteractor(iren)
         style_axial = CustomInteractorStyle(self.view_axial)
+        # style_axial = vtk.vtkInteractorStyleImage()
         style_axial.SetDefaultRenderer(self.view_axial.GetRenderer())
+        self.view_axial.SetupInteractor(iren)
+
         iren.SetInteractorStyle(style_axial)
 
         # self.view_coronal = vtk.vtkImageViewer2()
