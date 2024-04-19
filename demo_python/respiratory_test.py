56e2fc03fc4c9fd785fb71be06631455787897cc
zxliao <zhixiangleo@163.com>
2024-04-19 17:16:11 +0800

    feat：增加穿刺指令数据采集

--- demo_python/respiratory_test.py
+++ demo_python/respiratory_test.py
@@ -7,36 +7,47 @@
 import laser_rangefinder
 import serial_communication
 import respiratory_force_sensor
+from robot_mqtt_client import RobotMQTTClient
 import time
 import sys
 import keyboard
 import csv
+import json
 from threading import Thread
-import threading
-import signal
 
-def signal_handler(signal, frame):
-    # 终止所有正在运行的线程
-    for thread in threading.enumerate():
-        if thread.is_alive():
-            thread.do_run = False
-    print('程序已结束')
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
+def in_range(value,range)->bool:
+    if value>=range[0] and value<=range[1]:
+        return True
+    else:
+        return False
+    
+def save_data_file(filename:str,data):
+    with open(filename,'w',newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+        csvfile.close()
+    print('create data file '+filename)
 
 class RespiratoryTest(object):
-    def __init__(self,laser,servo,rp_sensor) -> None:
+    def __init__(self,laser,servo,rp_sensor,mqtt_client) -> None:
         self.laser = laser
         self.servo = servo
         self.rp_sensor = rp_sensor
+        self.mqtt_client = mqtt_client
         self.distance_data = []
         self.respiratory_force = []
         self.needle_force = []
+        self.respiratory_command = []
         self.record_button = False
+        self.rp_force = None
         keyboard.on_press_key('t',self.start_record)
         keyboard.on_press_key('p',self.stop_record)
         keyboard.on_press_key('z',self.quit)
+        self.mqtt_client.add_callabck_robot_info(self.on_topic_robot_info)
+        self.mqtt_client.client.loop_start()
+
+    def on_topic_robot_info(self,client,userdata,msg):
+        pass
 
     def quit(self,x):
         sys.exit()
@@ -51,42 +62,56 @@ class RespiratoryTest(object):
     def stop_record(self,x):
         t = time.strftime('%Y-%m%d-%H%M%S',time.localtime())
         filename = './data/respiratorymotion_data_'+t+'.txt'
-        with open(filename,'w',newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(self.distance_data)
-            csvfile.close()
-        print('create data file '+filename)
+        save_data_file(filename,self.distance_data)
         filename = './data/needleforce_data_'+t+'.txt'
-        with open(filename,'w',newline='') as csvfile:
-            writer  =csv.writer(csvfile)
-            writer.writerows(self.needle_force)
-            csvfile.close()
-        print('create data file '+filename)
+        save_data_file(filename,self.needle_force)
         filename = './data/respiratoryforce_data_'+t+'.txt'
-        with open(filename,'w',newline='') as csvfile:
-            writer  =csv.writer(csvfile)
-            writer.writerows(self.respiratory_force)
-            csvfile.close()
-        print('create data file '+filename)
+        save_data_file(filename,self.respiratory_force)
+        filename = './data/respiratorycommand_data_'+t+'.txt'
+        save_data_file(filename,self.respiratory_command)
 
         print('complete recording!')
         self.distance_data.clear()
         self.respiratory_force.clear()
         self.needle_force.clear()
+        self.respiratory_command.clear()
         self.record_button = False
         
+    def respiratory_control(self):
+        pub_data = {"name":"respiratory","motion":False}
+        # range = [30970,31160]
+        # range = [0,30970]
+        range = [31160,50000]
+        while self.rp_force is None:
+            time.sleep(0.2)
+        self.rp_force_last = self.rp_force
+        while True:
+            if in_range(self.rp_force,range) and not in_range(self.rp_force_last,range):
+                pub_data['motion'] = True
+                self.mqtt_client.pub_hr_robot(json.dumps(pub_data))
+                if self.record_button:
+                    self.respiratory_command.append([time.time(),1])
+            if in_range(self.rp_force_last,range) and not in_range(self.rp_force,range):
+                pub_data['motion'] = False
+                self.mqtt_client.pub_hr_robot(json.dumps(pub_data))
+                if self.record_button:
+                    self.respiratory_command.append([time.time(),0])
+            self.rp_force_last = self.rp_force
+            time.sleep(0.005)
 
     def read_distance_data(self):
         while True:
             dis = self.laser.read_sensor_value()
             if self.record_button:
                 self.distance_data.append([time.time(),dis])
+            time.sleep(0.005)
 
     def read_respiratory_force(self):
         while True:
-            rp_force = self.rp_sensor.read_sensor_value()
+            self.rp_force = self.rp_sensor.read_sensor_value()
             if self.record_button:
-                self.respiratory_force.append([time.time(),rp_force])
+                self.respiratory_force.append([time.time(),self.rp_force])
+            time.sleep(0.01)
 
     def read_needle_force(self):
         while True:
@@ -97,19 +122,29 @@ class RespiratoryTest(object):
 
     def run(self):
         thread_distance = Thread(target=self.read_distance_data)
-        thread_respiratory_force = Thread(target=self.read_respiratory_force)
-        thread_needle_force = Thread(target=self.read_needle_force)
+        thread_distance.daemon = True
         thread_distance.start()
+
+        thread_respiratory_force = Thread(target=self.read_respiratory_force)
+        thread_respiratory_force.daemon = True
         thread_respiratory_force.start()
+
+        thread_needle_force = Thread(target=self.read_needle_force)
+        thread_needle_force.daemon = True
         thread_needle_force.start()
 
-        # while True:
-        #     time.sleep(1)
+        thread_respiratory_control = Thread(target=self.respiratory_control)
+        thread_respiratory_control.daemon = True
+        thread_respiratory_control.start()
+
+        while True:
+            time.sleep(1)
 
 
 if __name__=='__main__':
     laser = laser_rangefinder.laser_rangefinder(9600)
-    servo = serial_communication.SerialModbus(port='COM11',bps=9600,timex=0.1)
+    servo = serial_communication.SerialModbus(port='COM4',bps=9600,timex=0.1)
     rp_sensor = respiratory_force_sensor.respiratory_sensor(com='COM12',bps=9600)
-    rp_test = RespiratoryTest(laser,servo,rp_sensor)
+    mqtt_client = RobotMQTTClient('192.168.2.242')
+    rp_test = RespiratoryTest(laser,servo,rp_sensor,mqtt_client)
     rp_test.run()
