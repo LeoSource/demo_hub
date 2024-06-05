7ef1c96307e9c943ca39a569a8e8f531bc712162
Leo <zhixiangleo@163.com>
2024-06-05 21:46:24 +0800

    feat：动态显示三维曲线

--- demo_python/needle_display.py
+++ demo_python/needle_display.py
@@ -13,8 +13,10 @@ class NeedleGraph(object):
     def __init__(self,mqtt_client) -> None:
         self.pos_needle = None
         self.mqtt_client = mqtt_client
-        self.mqtt_client.add_callback_robot_info(self.on_topic_robot_info)
-        self.mqtt_client.client.loop_start()
+        # self.mqtt_client.add_callback_robot_info(self.on_topic_robot_info)
+        # self.mqtt_client.client.loop_start()
+        self.idx = 1
+        self.dir = 1.0
 
     def on_topic_robot_info(self,client,userata,msg):
         j = json.loads(msg.payload)
@@ -24,8 +26,15 @@ class NeedleGraph(object):
         self.pos_needle = self.pos_needle.T
 
     def plot(self):
+        self.pos_needle = np.array([[0,0,0],[1,2,3]],dtype=np.float64)
+        self.pos_needle = self.pos_needle.T
+        self.p1 = np.array([[0,0,0],[1,2,0]],dtype=np.float64)
+        self.p1 = self.p1.T
+
         fig = plt.figure()
         ax = fig.add_subplot(111,projection='3d')
+        ax.set(xlabel='X',ylabel='Y',zlabel='Z')
+        ax.view_init(azim=-90,elev=90)
 
         timer = fig.canvas.new_timer(100)
         timer.add_callback(self.__on_timer,ax)
@@ -33,13 +42,21 @@ class NeedleGraph(object):
         plt.show()
 
     def __on_timer(self,ax):
+        if self.idx%20==0:
+            self.dir = -1*self.dir
+        self.pos_needle[0,1] = self.pos_needle[0,1]+0.5*self.dir
+        self.idx = self.idx+1
+        print(self.pos_needle[:,1])
         if self.pos_needle is not None:
             ax.plot(self.pos_needle[0,:],self.pos_needle[1,:],self.pos_needle[2,:],'bo-',linewidth=3)
+            ax.plot(self.p1[0,:],self.p1[1,:],self.p1[2,:],'go--',linewidth=1)
+            plt.draw()
 
 
 if __name__=='__main__':
-    mqtt_client = RobotMQTTClient('192.168.2.242')
-    needle = NeedleGraph(mqtt_client)
+    # mqtt_client = RobotMQTTClient('192.168.2.242')
+    # needle = NeedleGraph(mqtt_client)
+    needle = NeedleGraph(1)
     needle.plot()
 
 
