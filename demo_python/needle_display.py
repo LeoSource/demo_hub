0e2dd1a6b7692c7dc17b0e646350e5c5fce074b9
zxliao <zhixiangleo@163.com>
2024-06-06 13:26:05 +0800

    fix：通过animation实现三维动态曲线显示

--- demo_python/needle_display.py
+++ demo_python/needle_display.py
@@ -5,9 +5,15 @@
 # @Author	    : zxliao, zhixiangleo@163.com
 
 from robot_mqtt_client import RobotMQTTClient
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+
+
+def frame_gen():
+    while True:
+        yield
 
 class NeedleGraph(object):
     def __init__(self,mqtt_client) -> None:
@@ -25,54 +31,43 @@ class NeedleGraph(object):
         self.pos_needle = np.array([p1,p2])
         self.pos_needle = self.pos_needle.T
 
-    def plot(self):
+    def animate(self):
         self.pos_needle = np.array([[0,0,0],[1,2,3]],dtype=np.float64)
         self.pos_needle = self.pos_needle.T
         self.p1 = np.array([[0,0,0],[1,2,0]],dtype=np.float64)
         self.p1 = self.p1.T
 
         fig = plt.figure()
-        ax = fig.add_subplot(111,projection='3d')
+        ax = plt.subplot(111,projection='3d')
+        self.line2, = ax.plot(self.pos_needle[0,:],self.pos_needle[1,:],self.pos_needle[2,:],'bo-',linewidth=3)
+        self.line1, = ax.plot(self.p1[0,:],self.p1[1,:],self.p1[2,:],'go--',linewidth=2)
         ax.set(xlabel='X',ylabel='Y',zlabel='Z')
         ax.view_init(azim=-90,elev=90)
+        ax.set_proj_type('ortho')  # 设置为正交投影，以减少透视失真
+        ax.set_xlim([0,4])
+        fig.tight_layout()
 
-        timer = fig.canvas.new_timer(100)
-        timer.add_callback(self.__on_timer,ax)
-        timer.start()
+        ani = FuncAnimation(fig, self.update, frames=frame_gen, interval=50, blit=True)
+
         plt.show()
 
-    def __on_timer(self,ax):
+    def update(self,frame):
         if self.idx%20==0:
             self.dir = -1*self.dir
-        self.pos_needle[0,1] = self.pos_needle[0,1]+0.5*self.dir
+        self.pos_needle[0,1] = self.pos_needle[0,1]+0.1*self.dir
         self.idx = self.idx+1
-        print(self.pos_needle[:,1])
-        if self.pos_needle is not None:
-            ax.plot(self.pos_needle[0,:],self.pos_needle[1,:],self.pos_needle[2,:],'bo-',linewidth=3)
-            ax.plot(self.p1[0,:],self.p1[1,:],self.p1[2,:],'go--',linewidth=1)
-            plt.draw()
+    
+        self.line2.set_data(self.pos_needle[0,:], self.pos_needle[1,:])
+        self.line2.set_3d_properties(self.pos_needle[2,:])
+        self.line1.set_data(self.p1[0,:],self.p1[1,:])
+        self.line1.set_3d_properties(self.p1[2,:])
+        return [self.line1,self.line2]
+
 
 
 if __name__=='__main__':
     # mqtt_client = RobotMQTTClient('192.168.2.242')
     # needle = NeedleGraph(mqtt_client)
     needle = NeedleGraph(1)
-    needle.plot()
-
-
-    # p1 = np.array([0,0,0])
-    # p2 = np.array([1,2,3])
-    # pos = np.array([p1,p2])
-    # pos = pos.T
-    # print(pos[0,:])
-    # print(pos[1,:])
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111,projection='3d')
-    # ax.plot(pos[0,:],pos[1,:],pos[2,:],'bo-',linewidth=3)
-    # ax.set_xlim(-1,1)
-    # ax.set_ylim(-2,2)
-    # ax.set_zlim(-3,3)
-    # ax.set_xlabel('X')
-    # ax.set_ylabel('Y')
-    # ax.set_zlabel('Z')
-    # plt.show()
+    # needle.plot()
+    needle.animate()
